393cdcf2343bc59871924d27ce10221f5e405f27
webbytkx <akeats0827@gmail.com>
2025-01-13 20:02:22 -0500

    Updated Player and main.py to add forward and backward movement

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import pygame
 from constants import *
+from player import Player
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+
+Player.containers = (updatable, drawable)
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for tri in updatable:
+            tri.update(dt)
 
         screen.fill("black")
+        
+        for tri in drawable:
+            tri.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
