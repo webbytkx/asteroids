a651d1a8c46b624db25dd3872221ab392426787d
webbytkx <akeats0827@gmail.com>
2025-01-14 20:19:33 -0500

    Inital commit

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+shots = pygame.sprite.Group()
+
+Asteroid.containers = (asteroids, updatable, drawable)
+Shot.containers = (shots, updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = updatable
+asteroid_field = AsteroidField()
 
 Player.containers = (updatable, drawable)
 
@@ -21,6 +33,16 @@ def main():
         
         for tri in updatable:
             tri.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collide(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collide(shot):
+                    shot.kill()
+                    asteroid.split()
 
         screen.fill("black")
         
